05f0d79867dfa1cb40270fa97c1d7eb672cf2211
GitmeTurnam <bulentkoparal@hotmail.com>
2019-03-28 21:16:21 +0300

    Update JavaApplication29.java

--- JavaApplication29/src/javaapplication29/JavaApplication29.java
+++ JavaApplication29/src/javaapplication29/JavaApplication29.java
@@ -15,6 +15,7 @@ public class JavaApplication29 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        kjhkh
         // TODO code application logic here
     }
     
