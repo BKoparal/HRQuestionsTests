a9d5e3edbef0354f6bf5dd4ee3a50df29e2fce2f
GitmeTurnam <gitmeturnam@users.noreply.github.com>
2019-03-29 01:24:24 +0300

    UcSayiyiSiralama

---
+++ UcSayiyiSİralama.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ucsayiyisiralama;
+
+/**
+ *
+ * @author xx
+ */
+public class UcSayiyiSiralama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        int a,b,c,temp;
+        a=9;b=4;c=2;
+        
+        //a ile b den küçüğü bulup a'ya atayalım
+        if (a>b){
+            temp=a;
+            a=b;
+            b=temp;}// şimdi a da a ile b arasında küçük olan var
+        
+        //ikinci if
+        if(a>c){
+            temp=a;
+            a=c;
+            c=temp;} // artık a da kesinlikle en küçük sayı var
+        //işimiz artık çok kolay sadece b ile c den küçük olanı bulacağız
+        
+        if(b>c){
+            temp=b;
+            b=c;
+            c=temp;}
+        
+        //ve bitti sadece yazdır
+        System.out.println(a+""+b+""+c);
+    }   
+    
+}
